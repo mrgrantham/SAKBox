48508df2786b45d04ca70e6d1e72792e7bce2d99
James Grantham <james.grantham@gmail.com>
2023-02-26 11:47:37 -0800

    update example

--- projects/examples/sdl2/src/exampleSDL2.cpp
+++ projects/examples/sdl2/src/exampleSDL2.cpp
@@ -23,25 +23,31 @@ int main()
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    const int windowHeight = 480;
+    const int windowWidth = 680;
     SDL_Window *window = SDL_CreateWindow("SDL2 Window",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
+                                          windowWidth, windowHeight,
                                           window_flags);
-
+    SDL_Renderer *sdlRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer,
+										SDL_PIXELFORMAT_ARGB8888,
+										SDL_TEXTUREACCESS_STREAMING,
+										windowWidth, windowHeight);
     if(!window)
     {
         fmt::println("Failed to create window");
         return -1;
     }
 
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    // SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 
-    if(!window_surface)
-    {
-        fmt::println("Failed to get the surface from the window");
-        return -1;
-    }
+    // if(!window_surface)
+    // {
+    //     fmt::println("Failed to get the surface from the window");
+    //     return -1;
+    // }
     bool done = false;
     while (!done)
     {
@@ -53,8 +59,26 @@ int main()
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
                 done = true;
         }
-        SDL_UpdateWindowSurface(window);
+        int *pixels = nullptr;
+		int pitch = 0;
+        const SDL_Rect* SDL_LOCK_ENTIRE_TEXTURE = nullptr;
+        if ( SDL_LockTexture(sdlTexture,SDL_LOCK_ENTIRE_TEXTURE,(void **)&pixels,&pitch) < 0 )
+		{
+			fmt::println("ERROR");
+			return 1;
+		}
+        // SDL_UpdateWindowSurface(window);
+        SDL_SetRenderTarget(sdlTexture)
+        SDL_UnlockTexture(sdlTexture);
+    	SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, NULL);
+	    // Update the whole screen
+	    SDL_RenderPresent(sdlRenderer);
     }
 
+    // Cleanup
+    SDL_DestroyTexture(sdlTexture);
+    SDL_DestroyRenderer(sdlRenderer);
     // SDL_Delay(5000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
